85d4b77664237d2961ab92678639eb35413dc5fa
Santiago Lopez <santiago8695sld@gmail.com>
2024-12-13 05:55:44 -0300

    Agregando comentarios y explicaciones

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Molde de los productos
 class Producto {
     constructor(imagen, nombreAlbum, autor, precio) {
         this.imagen = imagen;
@@ -7,9 +8,8 @@ class Producto {
     }
 }
 
-const catalogo = [
-
-];
+//Arreglo del catalogo y posteriormente la cracion de los productos en base al molde y como los cargue al arreglo
+const catalogo = [];
 
 const producto1 = new Producto("img/flowerBoy.jpg", "Flower Boy", "Tyler, The Creator", 27);
 const producto2 = new Producto("img/blond.jpeg", "Blond", "Frank Ocean", 34);
@@ -18,7 +18,7 @@ const producto4 = new Producto("img/TheMelodicBlueCover.jpeg", "Melodic Blue", "
 const producto5 = new Producto("img/CMIYGLTES.jpg", "Call Me If You Get Lost", "Tyler, The Creator", 30);
 const producto6 = new Producto("img/CO.jpg", "channel, ORANGE", "Frank Ocean", 24);
 const producto7 = new Producto("img/damn.jpg", "DAMN.", "Kendrick Lamar", 34);
-const producto8 = new Producto("img/dieformybitch.jpg", "Die For My Bitch", "Baby Keem", 17);
+const producto8 = new Producto("img/Dieformybitch.jpg", "Die For My Bitch", "Baby Keem", 17);
 
 catalogo.push(producto1);
 catalogo.push(producto2);
@@ -29,9 +29,14 @@ catalogo.push(producto6);
 catalogo.push(producto7);
 catalogo.push(producto8);
 
+//Carrito de compras donde se guarda lo que agreguemos (usa localStorage asi si se cierra o reinicia la pagina se mantienen los articulos en el carrito)
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-let carrito = [];
+function guardarCarrito() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
 
+//funcion para cargar el catalogo, por cada producto(objeto) en el catalogo crea una tarjeta con su respectiva informacion y luego por medio de id consigue el contenedor para cargarlo ahi (el que se menciona su funcion en el html)
 function mostrarCatalogo(catalogo) {
     const contenedor = document.getElementById("contenedorCatalogo");
 
@@ -39,12 +44,12 @@ function mostrarCatalogo(catalogo) {
         const tarjetaDisco = document.createElement("div");
 
         tarjetaDisco.innerHTML = `
-					<img src="${producto.imagen}">
-					<h4>${producto.nombreAlbum}</h4>
-                    <h4>${producto.autor}</h4>
-                    <h4>$${producto.precio}</h4>
-					<button id="${producto.nombreAlbum}">Comprar</button>
-                    `
+            <img src="${producto.imagen}">
+            <h4>${producto.nombreAlbum}</h4>
+            <h4>${producto.autor}</h4>
+            <h4>$${producto.precio}</h4>
+            <button id="${producto.nombreAlbum}">Comprar</button>
+        `;
 
         contenedor.appendChild(tarjetaDisco);
 
@@ -53,6 +58,7 @@ function mostrarCatalogo(catalogo) {
     });
 }
 
+//funcion para "comprar" o agregar a la lista de compras en si
 function agregarAlCarrito(producto) {
     const productoExistente = carrito.find(item => item.nombreAlbum === producto.nombreAlbum);
 
@@ -62,17 +68,18 @@ function agregarAlCarrito(producto) {
         carrito.push({ ...producto, cantidad: 1 });
     }
 
-    mostrarCarrito();
+    guardarCarrito(); 
+    mostrarCarrito(); 
 }
 
+//formato de la tabla en la que se muestra la compra (recibo)
 function mostrarCarrito() {
     const listaCarrito = document.getElementById("listaCarrito");
     const totalCarrito = document.getElementById("totalCarrito");
-    const comprar = document.getElementById('btnCompra');
 
     listaCarrito.innerHTML = "";
 
-    const tabla = document.createElement('table');
+    const tabla = document.createElement("table");
 
     tabla.innerHTML = `
         <tr>
@@ -100,18 +107,28 @@ function mostrarCarrito() {
         `;
 
         tabla.appendChild(fila);
-
     });
 
     listaCarrito.appendChild(tabla);
 
-    totalCarrito.innerText = `Total: $${total}`;   
+    totalCarrito.innerText = `Total: $${total}`;
 }
 
-
+//funcion para cuando queremos remover un producto del carrito
 function eliminarDelCarrito(nombreAlbum) {
     carrito = carrito.filter(item => item.nombreAlbum !== nombreAlbum);
-    mostrarCarrito();
+    guardarCarrito(); 
+    mostrarCarrito(); 
 }
 
+//funcion que recupera el carrito del localStorage
+function recuperarCarrito() {
+    const carritoRecuperado = localStorage.getItem("carrito");
+    if (carritoRecuperado) {
+        carrito = JSON.parse(carritoRecuperado);
+        mostrarCarrito();
+    }
+}
+
+recuperarCarrito(); 
 mostrarCatalogo(catalogo);
